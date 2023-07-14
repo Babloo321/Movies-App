cac3d77ede64f73f03e5760357b0348d28d4cbec
Babloo Kumar <babloocse888@gmail.com>
2023-07-14 22:50:13 +0530

    :tada:started movies app

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div style={{background:"lightblue",padding:'1rem',fontFamily:"sans-serif", fontWeight:"bold", display:"flex", justifyContent:"center", alignItems:"center"}}>
+        <h1>Movies App</h1>
+        <h2 style={{marginLeft:"2rem"}}>Favourite</h2>
+      </div>
+    )
+  }
+}
