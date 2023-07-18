5aed3916c36cb15ad9e3aed564f8ae8a23524962
Babloo Kumar <babloocse888@gmail.com>
2023-07-18 09:02:07 +0530

    :construction: react-route-dom

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 export default class Navbar extends Component {
   render() {
     return (
       <div style={{background:"white", color:"darkblue",padding:'1rem',fontFamily:"sans-serif", fontWeight:"bold", display:"flex", justifyContent:"center", alignItems:"center" }}>
+        <Link to='/' style={{textDecoration:"none"}}>
         <h1>Movies App</h1>
+        </Link>
+        <Link to='/fav' style={{textDecoration:"none"}}>
         <h2 style={{marginLeft:"2rem"}}>Favourite</h2>
+        </Link>
       </div>
     )
   }
